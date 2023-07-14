0c76a8d614ac4f2289e7c245447186f904d658b4
timusss0 <mustikavia86@gmail.com>
2023-07-14 20:58:45 +0700

    good bang

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,6 +1,8 @@
-test("test sum func", () => {
+import { sum } from "../src/sum";
 
-    const result = sum(1,2)
+test("test sum func", () => {
+    const result = sum(1, 2)
+
 
      expect(result).toBe(3)
 
